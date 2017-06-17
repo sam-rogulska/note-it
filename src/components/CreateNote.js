1771738044ac06fa6b26b468e55f894c34df5d46
Sam Rogulska <samantha.rogulska@thehutgroup.com>
2017-06-17 17:13:22 +0100

    Add create note functionality

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import TitleInput from './TitleInput';
-import TextArea from './TextArea';
-import SubmitButton from './SubmitButton';
+import uuid from 'uuid';
 
 class CreateNote extends Component {
   constructor(props){
     super(props)
-    
+    var today = new Date(),
+        date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+    this.state = {inputValue: '', textValue: '', date: date, notes: JSON.parse(localStorage.getItem("notes"))};
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.handleChangeText = this.handleChangeText.bind(this);
+  }
+  handleChangeText(event, value) {
+    this.setState({textValue: event.target.value});
+  }
+  handleChangeInput(event) {
+    this.setState({inputValue: event.target.value});
+  }
+  addNote = () => {
+    const key = 'notes';
+    let notes;
+    if (this.state.notes !== null) {
+      notes = this.state.notes
+      notes.push({
+          id: uuid.v4(),
+          title: this.state.inputValue,
+          text: this.state.textValue,
+          createdDate: this.state.date
+        });
+      
+    }
+    else {
+      notes = [{
+          id: uuid.v4(),
+          title: this.state.inputValue,
+          text: this.state.textValue,
+          createdDate: this.state.date 
+      }];
+    }
+    this.setState({ notes: notes });
+    localStorage.setItem(key, JSON.stringify(notes));
   }
   render() {
  
@@ -15,11 +47,11 @@ class CreateNote extends Component {
             <div className="createNote-header">
               <h2>Create a new Note</h2>
             </div>
-            <div className="createNote-form">
-                <TitleInput/>
-                <TextArea/>
-                <SubmitButton/>
-            </div>
+            <form className="createNote-form">
+                <input type="text" className="titleInput" value={this.state.inputValue} onChange={this.handleChangeInput}></input>
+                <textarea className="textArea" value={this.state.textValue} onChange={this.handleChangeText}></textarea>
+                <button className="submitButton" onClick={this.addNote}>Add Note</button>
+            </form>
           </div>
         );
     }
